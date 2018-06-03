8c65701d4efdc5b51c90f741cb6e9d18a5cb76a1
joney000 <jaswantsinghyadav007@gmail.com>
2018-06-04 00:30:03 +0530

    update binary index tree

--- Algorithms/BIT.java
+++ Algorithms/BIT.java
@@ -4,418 +4,453 @@ import java.io.*;
 import java.math.*;
 
 /*
- * Coded by  	: jaswant Singh
- * Lang   		: Java
- * Algorithm 	: Not Specified
- * Date           : 29/Nov/2015
- */ 
- class Solution implements Comparable<Solution>{
-	 public int vertex;
-	 public int weight; 
-	 //public provide flexibility to access from outside the the class
-	 //at the cost of security
-	 public Solution(){
-		this.vertex = 0;
-		this.weight = 0;
-	 }
-	 public Solution(int node , int weight){
-		 this.vertex = node;
-		 this.weight = weight;
-	 }
-	 @Override
-	 public int compareTo(Solution e){
-		 if(this.weight<e.weight)return -1;
-		 else if(this.weight==e.weight) return 0;
-		 else return 1;
-	 }
-	 @Override
-	 public String toString(){
-	 	return " vertex = "+ this.vertex + " weight= "+this.weight ;
-	 }
-     //Fast Reader implementation , handles large I/O files
-    private boolean finished = false;
+ * Author    : joney_000[let_me_start]
+ * Algorithm : Bit / fenwick tree
+ * Platform  : Codeforces
+ * Ref       : https://sanugupta.wordpress.com/2014/08/29/binary-indexed-tree-fenwick-tree/
+ */
 
-	private InputStream stream;
-	private byte[] buf = new byte[1024];  //input Buffer
-	private int curChar;
-	private int numChars;
-	private SpaceCharFilter filter;		  
+public class A{ 
+  
+  private InputStream inputStream ;
+  private OutputStream outputStream ;
+  private FastReader in ;
+  private PrintWriter out ;
+  
+  private final int BUFFER = 200005;
+  
+  private int    auxInts[] = new int[BUFFER];
+  private long   auxLongs[] = new long[1];
+  private double auxDoubles[] = new double[1];
+  private char   auxChars[] = new char[1];
+  private final long mod = 1000000000+7;
+  private final int  INF  = Integer.MAX_VALUE;
+  private final long INF_L  = Long.MAX_VALUE / 10;
 
-	public Solution(InputStream stream){
-		this.stream = stream;
-	}
-    public static InputStream inputStream = System.in;
-	public static OutputStream outputStream = System.out;
-	public static Solution in = new Solution(inputStream);
-    public static PrintWriter out = new PrintWriter(outputStream);
-	/*
-	 *  Overhead [Additional Temporary Storage] 
-	 *  But it save a lot of time and Re Allocation of Space . it Reuse The same Buffers for all the Test Cases.
-	 *  take care of Limit : 10^5 + 5 (Runtime Error:OverFlow)
-	 */
-	public static int tempints[] = new int[100005];
-	public static long templongs[] = new long[100005];
-	public static double tempdoubles[] = new double[100005];
-	public static char tempchars[] = new char[100005];
-	public static long mod = 1000000000+7;
- 	public static LinkedList<Integer> adj[];
-			
-	
-		    //Creating Reader-Writer Buffer of initial size 2000B
-			//public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in),2000);
-		//	public static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out),2000);			
-	public static long INF = Integer.MAX_VALUE/1000;
-			
-				
-
-	public static void main(String []arg)throws Exception{
-			
-			//int tests = i();
-		//	for(int t = 1 ; t <= tests; t++){
-				
-				int n = i();
-				int freq[] = is(n);
-				long tree[] = new long[n+100];
-				//Building the tree
-				for(int i =1 ; i<= n ; i++){
-					update(tree ,i , freq[i]);
-				}
-				int q = i();
-				for(int i =1 ; i <= q ; i++){
-					int idx = i();
-					long ans = query(tree ,idx);
-				 	out.write(""+ans+"\n");	out.flush();	
-				}
-				
-		//	}		
-			out.flush();
-			return ;
-		}
-	
-		
-//                   My General Utilities for Contest                //
- void update(long []tree , int idx , int val){
-     
-     while (idx < tree.length){
-        tree[idx] += val;
-        idx += (idx & -idx);
+  public A(){}
+  public A(boolean stdIO)throws FileNotFoundException{
+    // stdIO = false;
+    if(stdIO){
+      inputStream = System.in;
+      outputStream = System.out;
+    }else{
+      inputStream = new FileInputStream("input.txt");
+      outputStream = new FileOutputStream("output.txt");
     }
-}
- long query(long []tree , int idx){
-     
-    long sum = 0;
-    while (idx > 0){
-        sum += tree[idx];
-        idx -= (idx & -idx);
+    in = new FastReader(inputStream);
+    out = new PrintWriter(outputStream);
+  }
+  
+  
+  void run()throws Exception{
+    int n = i(); int q = i(); int m = i();
+    int qry[][] = new int[3][q + 1];// 0=>l, 1=> r, 2=>val
+    int a[] = new int[n + 1];
+
+    for(int i = 1; i <= n; i++){
+      a[i] = i();
+      update(tree, i, a[i]);				// point update this time, 
+      update(tree, i + 1, -a[i]);   // if range qry than comment this line and res = qry(tree, idx) - qry(tree, idx - 1)
     }
-    return sum;
+    
+    for(int i = 1; i <= q; i++){
+      qry[0][i] = i(); // l
+      qry[1][i] = i(); // r
+      qry[2][i] = i(); // value
+    }
+    
+    for(int i = 1; i <= q; i++){
+      update(tree, qry[0][i], qry[2][i]);
+      update(tree, qry[1][i] + 1, qry[2][i]); // range [l , r]
+    }
+
+    for(int i = 1; i <= n; i++){
+      long res = qry(tree, i); // case of point qry
+      out.write(""+res+" ");		
+    }
+
+  }// end run
+
+  final int MAX = 200000;
+  long tree[] = new long[MAX + 5];  
+  
+  void update(long tree[], int idx, long value){		// idx to MAX
+    while(idx < MAX){
+      tree[idx] += value;
+      idx += (idx & (-idx));
+    }
+  }
+  
+  long qry(long tree[], int idx){
+    long ret = 0;
+    while(idx > 0){
+      ret += tree[idx];
+      idx -= (idx & (-idx));
+    }
+    return ret;
+  }
+
+  long gcd(long a, long b){
+    if(b == 0)return a;
+    return gcd(b, a % b);
+  }
+
+  long lcm(long a, long b){
+    if(a == 0 || b == 0)return 0;
+    return (a * b)/gcd(a, b);
+  }
+
+  long mulMod(long a, long b, long mod){
+    if(a == 0 || b == 0)return 0;
+    if(b == 1)return a;
+    long ans = mulMod(a, b/2, mod);
+    ans = (ans * 2) % mod;
+    if(b % 2 == 1)ans = (a + ans)% mod;
+    return ans;
+  }
+
+  long pow(long a, long b, long mod){
+    if(b == 0)return 1;
+    if(b == 1)return a;
+    long ans = pow(a, b/2, mod);
+    ans = (ans * ans);
+    if(ans >= mod)ans %= mod;
+
+    if(b % 2 == 1)ans = (a * ans);
+    if(ans >= mod)ans %= mod;
+
+    return ans;
+  }
+
+  // 20*20   nCr Pascal Table
+  long[][] ncrTable(){
+    long ncr[][] = new long[21][21];
+
+    for(int i = 0; i <= 20; i++){
+      ncr[i][0] = ncr[i][i] = 1L;
+    }
+
+    for(int j = 0; j <= 20; j++){
+      for(int i = j + 1; i <= 20; i++){
+        ncr[i][j] = ncr[i-1][j] + ncr[i-1][j-1];
+      }
+    }
+
+    return ncr;
+  }
+
+  int i()throws Exception{
+    return in.nextInt();
+  }
+
+  int[] is(int n)throws Exception{
+    for(int i=1 ; i <= n ;i++)auxInts[i] = in.nextInt();  
+      return auxInts;
+  }
+
+  long l()throws Exception{
+    return in.nextLong();
+  }
+
+  long[] ls(int n)throws Exception{
+    for(int i=1 ; i <= n ;i++)auxLongs[i] = in.nextLong();  
+      return auxLongs;
+  }
+
+  double d()throws Exception{
+    return in.nextDouble();
+  }
+
+  double[] ds(int n)throws Exception{
+    for(int i=1 ; i <= n ;i++)auxDoubles[i] = in.nextDouble();  
+      return auxDoubles;
+  }
+
+  char c()throws Exception{
+    return in.nextCharacter();
+  }
+
+  char[] cs(int n)throws Exception{
+    for(int i=1 ; i <= n ;i++)auxChars[i] = in.nextCharacter();  
+      return auxChars;
+  }
+
+  String s()throws Exception{
+    return in.nextLine();
+  }
+
+  BigInteger bi()throws Exception{
+    return in.nextBigInteger();
+  }
+
+  private void closeResources(){
+    out.flush();
+    out.close();
+    return;
+  }
+
+//  IMP: roundoff upto 2 digits 
+//  double roundOff = Math.round(a * 100.0) / 100.0;
+//                    or
+//  System.out.printf("%.2f", val);
+
+//  print upto 2 digits after decimal
+//  val = ((long)(val * 100.0))/100.0;    
+
+  public static void main(String[] args) throws java.lang.Exception{
+  
+    A driver = new A(true);
+    driver.run();
+    driver.closeResources();
+  }
 }
 
- 
- public static boolean isPrime(long n)throws Exception{
-	if(n==1)return false;
-	if(n<=3)return true;
-	if(n%2==0)return false;
-	for(int i=2 ;i <= Math.sqrt(n); i++){
-		if(n%i==0)return false;
-	}
-	return true;
- }
- public static long gcd (long a , long b)throws Exception{
-	if(b==0)return a;
-	return gcd(b , a%b);
- }
- public static long lcm (long a , long b)throws Exception{
-	if(a==0||b==0)return 0;
-	return (a*b)/gcd(a,b);         //TAKE CARE OF Integer RANGE OVERFLOW
- }
- public static long mulmod(long a , long b ,long mod)throws Exception{
-	if(a==0||b==0)return 0;
-	if(b==1)return a;
-	long ans = mulmod(a,b/2,mod);
-	ans = (ans*2)% mod;
-	if(b%2==1)ans = (a + ans)% mod;
-	return ans;
- }
- public static long pow(long a , long b ,long mod)throws Exception{
-	if(b==0)return 1;
-	if(b==1)return a;
-	long ans = pow(a,b/2,mod);
-	ans = (ans * ans)% mod;
-	if(b%2==1)ans = (a * ans)% mod;
-	return ans;
- }
- // 20*20   nCr Pascal Table
- public static long[][] ncrTable()throws Exception{
-	long ncr[][] = new long[21][21];
-	for(int i=0 ;i<=20 ;i++){ncr[i][0]=1;ncr[i][i]=1;}
-	for(int j=0;j<=20 ;j++){
-		for(int i=j+1;i<= 20 ;i++){
-			ncr[i][j] = ncr[i-1][j]+ncr[i-1][j-1];
-		}
-	}
-	return ncr;
- }
-//							My I/O function                    	//
-public static int i()throws Exception{
- return in.nextInt();                   					   //read a single Integer
-}
-public static int[] is(int n)throws Exception{               
-  for(int i=1 ; i <= n ;i++)tempints[i] = in.nextInt();        //read N integers
- return tempints;
-}
-public static long l()throws Exception{                        //read a single Long
- return in.nextLong();
-}
-public static long[] ls(int n)throws Exception{				   //read N Long type digits
-  for(int i=1 ; i <= n ;i++)templongs[i] = in.nextLong();  
- return templongs;
+class FastReader{
+
+  private boolean finished = false;
+
+  private InputStream stream;
+  private byte[] buf = new byte[4 * 1024];
+  private int curChar;
+  private int numChars;
+  private SpaceCharFilter filter;
+
+  public FastReader(InputStream stream){
+    this.stream = stream;
+  }
+
+  public int read(){
+    if (numChars == -1){
+      throw new InputMismatchException ();
+    }
+    if (curChar >= numChars){
+      curChar = 0;
+      try{
+        numChars = stream.read (buf);
+      } catch (IOException e){
+        throw new InputMismatchException ();
+      }
+      if (numChars <= 0){
+        return -1;
+      }
+    }
+    return buf[curChar++];
+  }
+
+  public int peek(){
+    if (numChars == -1){
+      return -1;
+    }
+    if (curChar >= numChars){
+      curChar = 0;
+      try{
+        numChars = stream.read (buf);
+      } catch (IOException e){
+        return -1;
+      }
+      if (numChars <= 0){
+        return -1;
+      }
+    }
+    return buf[curChar];
+  }
+
+  public int nextInt(){
+    int c = read ();
+    while (isSpaceChar (c))
+      c = read ();
+    int sgn = 1;
+    if (c == '-'){
+      sgn = -1;
+      c = read ();
+    }
+    int res = 0;
+    do{
+      if(c==','){
+        c = read();
+      }
+      if (c < '0' || c > '9'){
+        throw new InputMismatchException ();
+      }
+      res *= 10;
+      res += c - '0';
+      c = read ();
+    } while (!isSpaceChar (c));
+    return res * sgn;
+  }
+
+  public long nextLong(){
+    int c = read ();
+    while (isSpaceChar (c))
+      c = read ();
+    int sgn = 1;
+    if (c == '-'){
+      sgn = -1;
+      c = read ();
+    }
+    long res = 0;
+    do{
+      if (c < '0' || c > '9'){
+        throw new InputMismatchException ();
+      }
+      res *= 10;
+      res += c - '0';
+      c = read ();
+    } while (!isSpaceChar (c));
+    return res * sgn;
+  }
+
+  public String nextString(){
+    int c = read ();
+    while (isSpaceChar (c))
+      c = read ();
+    StringBuilder res = new StringBuilder ();
+    do{
+      res.appendCodePoint (c);
+      c = read ();
+    } while (!isSpaceChar (c));
+    return res.toString ();
+  }
+
+  public boolean isSpaceChar(int c){
+    if (filter != null){
+      return filter.isSpaceChar (c);
+    }
+    return isWhitespace (c);
+  }
+
+  public static boolean isWhitespace(int c){
+    return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+  }
+
+  private String readLine0(){
+    StringBuilder buf = new StringBuilder ();
+    int c = read ();
+    while (c != '\n' && c != -1){
+      if (c != '\r'){
+        buf.appendCodePoint (c);
+      }
+      c = read ();
+    }
+    return buf.toString ();
+  }
+
+  public String nextLine(){
+    String s = readLine0 ();
+    while (s.trim ().length () == 0)
+      s = readLine0 ();
+    return s;
+  }
+
+  public String nextLine(boolean ignoreEmptyLines){
+    if (ignoreEmptyLines){
+      return nextLine ();
+    }else{
+      return readLine0 ();
+    }
+  }
+
+  public BigInteger nextBigInteger(){
+    try{
+      return new BigInteger (nextString ());
+    } catch (NumberFormatException e){
+      throw new InputMismatchException ();
+    }
+  }
+
+  public char nextCharacter(){
+    int c = read ();
+    while (isSpaceChar (c))
+      c = read ();
+    return (char) c;
+  }
+
+  public double nextDouble(){
+    int c = read ();
+    while (isSpaceChar (c))
+      c = read ();
+    int sgn = 1;
+    if (c == '-'){
+      sgn = -1;
+      c = read ();
+    }
+    double res = 0;
+    while (!isSpaceChar (c) && c != '.'){
+      if (c == 'e' || c == 'E'){
+        return res * Math.pow (10, nextInt ());
+      }
+      if (c < '0' || c > '9'){
+        throw new InputMismatchException ();
+      }
+      res *= 10;
+      res += c - '0';
+      c = read ();
+    }
+    if (c == '.'){
+      c = read ();
+      double m = 1;
+      while (!isSpaceChar (c)){
+        if (c == 'e' || c == 'E'){
+          return res * Math.pow (10, nextInt ());
+        }
+        if (c < '0' || c > '9'){
+          throw new InputMismatchException ();
+        }
+        m /= 10;
+        res += (c - '0') * m;
+        c = read ();
+      }
+    }
+    return res * sgn;
+  }
+
+  public boolean isExhausted(){
+    int value;
+    while (isSpaceChar (value = peek ()) && value != -1)
+      read ();
+    return value == -1;
+  }
+
+  public String next(){
+    return nextString ();
+  }
+
+  public SpaceCharFilter getFilter(){
+    return filter;
+  }
+
+  public void setFilter(SpaceCharFilter filter){
+    this.filter = filter;
+  }
+
+  public interface SpaceCharFilter{
+    public boolean isSpaceChar(int ch);
+  }
 }
 
-public static double d()throws Exception{
-  return in.nextDouble();
-}
-public static double[] ds(int n)throws Exception{
-  for(int i=1 ; i <= n ;i++)tempdoubles[i] = in.nextDouble();  
- return tempdoubles;
-}
-public static char c()throws Exception{
- return in.nextCharacter();
-}
-public static char[] cs(int n)throws Exception{
-  for(int i=1 ; i <= n ;i++)tempchars[i] = in.nextCharacter();  
- return tempchars;
-}
-public static String s()throws Exception{
- return in.nextLine();
-}
-public static BigInteger bi()throws Exception{
- return in.nextBigInteger();
-}
-
-//*********************** for 0.2%f [precision data]***********************//
- /* 
-  * roundoff upto 2 digits 
-  * double roundOff = Math.round(a * 100.0) / 100.0;
-  *                  or
-  * System.out.printf("%.2f", val);
-  *				
-  */
-/*
-  *print upto 2 digits after decimal
-  *val = ((long)(val * 100.0))/100.0;
-  *
-*/
-
+class Pair implements Comparable<Pair>{
+  public int a;
+  public int b;
    
-	public int read(){
-		if (numChars == -1){
-			throw new InputMismatchException ();
-		}
-		if (curChar >= numChars){
-			curChar = 0;
-			try{
-				numChars = stream.read (buf);
-			} catch (IOException e){
-				throw new InputMismatchException ();
-			}
-			if (numChars <= 0){
-				return -1;
-			}
-		}
-		return buf[curChar++];
-	}
-	
-	public int peek(){
-		if (numChars == -1){
-			return -1;
-		}
-		if (curChar >= numChars){
-			curChar = 0;
-			try{
-				numChars = stream.read (buf);
-			} catch (IOException e){
-				return -1;
-			}
-			if (numChars <= 0){
-				return -1;
-			}
-		}
-		return buf[curChar];
-	}
+  public Pair(){
+    this.a = 0;
+    this.b = 0;
+  }
 
-	public int nextInt(){
-		int c = read ();
-		while (isSpaceChar (c))
-			c = read ();
-		int sgn = 1;
-		if (c == '-'){
-			sgn = -1;
-			c = read ();
-		}
-		int res = 0;
-		do{
-			if(c==','){
-				c = read();
-			}
-			if (c < '0' || c > '9'){
-				throw new InputMismatchException ();
-			}
-			res *= 10;
-			res += c - '0';
-			c = read ();
-		} while (!isSpaceChar (c));
-		return res * sgn;
-	}
+  public Pair(int a,int b){
+    this.a = a;
+    this.b = b;
+  }
 
-	public long nextLong(){
-		int c = read ();
-		while (isSpaceChar (c))
-			c = read ();
-		int sgn = 1;
-		if (c == '-'){
-			sgn = -1;
-			c = read ();
-		}
-		long res = 0;
-		do{
-			if (c < '0' || c > '9'){
-				throw new InputMismatchException ();
-			}
-			res *= 10;
-			res += c - '0';
-			c = read ();
-		} while (!isSpaceChar (c));
-		return res * sgn;
-	}
+  public int compareTo(Pair p){
+    if(this.a == p.a){
+     return this.b - p.b;  
+   }
+   return this.a - p.a; 
+  }
 
-	public String nextString(){
-		int c = read ();
-		while (isSpaceChar (c))
-			c = read ();
-		StringBuilder res = new StringBuilder ();
-		do{
-			res.appendCodePoint (c);
-			c = read ();
-		} while (!isSpaceChar (c));
-		return res.toString ();
-	}
-
-	public boolean isSpaceChar(int c){
-		if (filter != null){
-			return filter.isSpaceChar (c);
-		}
-		return isWhitespace (c);
-	}
-
-	public static boolean isWhitespace(int c){
-		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-	}
-
-	private String readLine0(){
-		StringBuilder buf = new StringBuilder ();
-		int c = read ();
-		while (c != '\n' && c != -1){
-			if (c != '\r'){
-				buf.appendCodePoint (c);
-			}
-			c = read ();
-		}
-		return buf.toString ();
-	}
-
-	public String nextLine(){
-		String s = readLine0 ();
-		while (s.trim ().length () == 0)
-			s = readLine0 ();
-		return s;
-	}
-
-	public String nextLine(boolean ignoreEmptyLines){
-		if (ignoreEmptyLines){
-			return nextLine ();
-		}else{
-			return readLine0 ();
-		}
-	}
-
-
-	public BigInteger nextBigInteger(){
-		try{
-			return new BigInteger (nextString ());
-		} catch (NumberFormatException e){
-			throw new InputMismatchException ();
-		}
-	}
-
-	public char nextCharacter(){
-		int c = read ();
-		while (isSpaceChar (c))
-			c = read ();
-		return (char) c;
-	}
-
-	public double nextDouble(){
-		int c = read ();
-		while (isSpaceChar (c))
-			c = read ();
-		int sgn = 1;
-		if (c == '-'){
-			sgn = -1;
-			c = read ();
-		}
-		double res = 0;
-		while (!isSpaceChar (c) && c != '.'){
-			if (c == 'e' || c == 'E'){
-				return res * Math.pow (10, nextInt ());
-			}
-			if (c < '0' || c > '9'){
-				throw new InputMismatchException ();
-			}
-			res *= 10;
-			res += c - '0';
-			c = read ();
-		}
-		if (c == '.'){
-			c = read ();
-			double m = 1;
-			while (!isSpaceChar (c)){
-				if (c == 'e' || c == 'E'){
-					return res * Math.pow (10, nextInt ());
-				}
-				if (c < '0' || c > '9'){
-					throw new InputMismatchException ();
-				}
-				m /= 10;
-				res += (c - '0') * m;
-				c = read ();
-			}
-		}
-		return res * sgn;
-	}
-
-	public boolean isExhausted(){
-		int value;
-		while (isSpaceChar (value = peek ()) && value != -1)
-			read ();
-		return value == -1;
-	}
-
-	public String next(){
-		return nextString ();
-	}
-
-	public SpaceCharFilter getFilter(){
-		return filter;
-	}
-
-	public void setFilter(SpaceCharFilter filter){
-		this.filter = filter;
-	}
-
-	public interface SpaceCharFilter{
-		public boolean isSpaceChar(int ch);
-	}
-}
-
-
+  @Override
+  public String toString(){
+    return "a = " + this.a + " b = " + this.b;
+  }
+} 
