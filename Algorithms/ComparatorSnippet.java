b0cf1596377a5871c4e3b93faef180adad688168
joney000 <developer.jaswant@gmail.com>
2019-03-10 22:34:18 +0530

    add sample to comparator

---
+++ Algorithms/ComparatorSnippet.java
@@ -0,0 +1,27 @@
+class Developer{
+	private int age;
+	
+	public Developer(int age){
+		this.age = age;
+	}
+
+	void setAge(int age){
+		this.age = age;
+	}
+	public int getAge(){
+		return age;
+	}
+}
+
+public class ComparatorSnippet{
+
+	public static void main(String... s){
+		
+		Developer[] devs = 	Collections.sort(listDevs, new Comparator<Developer>() {
+			@Override
+			public int compare(Developer o1, Developer o2) {
+				return o1.getAge() - o2.getAge();
+			}
+		});
+	}
+}
