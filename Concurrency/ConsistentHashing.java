552b5b05a20c3b55207aa0e0cdacdf82e80dff04
joney000 <developer.jaswant@gmail.com>
2020-03-25 23:48:16 +0530

    add consistent hashing for distributed cache

--- Concurrency/ConsistentHashing.java
+++ Concurrency/ConsistentHashing.java
@@ -4,9 +4,9 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+
 /**
  * Author    : joney_000[ developer.jaswant@gmail.com ]
  * Algorithm : Consistent Hashing Circle
@@ -43,27 +43,27 @@ class Server<T> extends ConsistentHashDataNode<T>{
 
 class ConsistentHashing <K, V> {
 
-	private TreeMap<Long, V> circle;
-  private HashMap<V, List<String>> nodeListMap;
-  private int noOfAliasForEachServer;
+    private TreeMap<Long, V> circle;
+    private HashMap<V, List<String>> nodeListMap;
+    private int noOfAliasForEachServer;
 
-  public ConsistentHashing(int noOfAliasForEachServer){
-  	this.noOfAliasForEachServer = noOfAliasForEachServer;
-  	circle = new TreeMap<Long, V>();
-  	nodeListMap = new HashMap<V, List<String>>();
-  }
+    public ConsistentHashing(int noOfAliasForEachServer){
+    	this.noOfAliasForEachServer = noOfAliasForEachServer;
+    	circle = new TreeMap<Long, V>();
+    	nodeListMap = new HashMap<V, List<String>>();
+    }
 
-  void put(String key, V value){
-  	Long hash = getHash(key);
-  	circle.put(hash, value);
-  }
-  
-  V remove(String key){
-  	if(circle.containsKey(key)){
-  		return circle.remove(key);
-  	}
-  	return null;
-  }
+    void put(String key, V value){
+    	Long hash = getHash(key);
+    	circle.put(hash, value);
+    }
+
+    V remove(String key){
+    	if(circle.containsKey(key)){
+    		return circle.remove(key);
+    	}
+    	return null;
+    }
 
  	void addServer(K key, V value){
  		put(key.toString(), value);
@@ -74,91 +74,80 @@ class ConsistentHashing <K, V> {
  		}
  	}
   
-  void removeServer(K key){
- 		remove(key.toString());
- 		for(int replicaId = 0; replicaId < noOfAliasForEachServer; replicaId++){
- 			String keyStr = key.toString() + " replica ~ "+replicaId;
-  		remove(keyStr);
- 		}
- 	}
+    void removeServer(K key){
+        remove(key.toString());
+        for(int replicaId = 0; replicaId < noOfAliasForEachServer; replicaId++){
+            String keyStr = key.toString() + " replica ~ "+replicaId;
+            remove(keyStr);
+        }
+	}
 
-  public V getServerNode(String val) {
-    Long hashing = getHash(val);
-    SortedMap<Long, V> tail = circle.tailMap(hashing);
-    return circle.get(tail.size() == 0 ? circle.firstKey() : tail.firstKey());
-  }
+    V getServerNode(String val) {
+        Long hashing = getHash(val);
+        SortedMap<Long, V> tail = circle.tailMap(hashing);
+        return circle.get(tail.size() == 0 ? circle.firstKey() : tail.firstKey());
+    }
 
 	public static void main(String ... args){
 		try{
 			ConsistentHashing<String, ConsistentHashDataNode<String>> cHash = new ConsistentHashing<>(5);
-
 			List <ConsistentHashDataNode<String>> servers = new LinkedList<>();
-			for(int i = 0; i < 4; i++){
-				ConsistentHashDataNode<String> newServer = new Server<String>("server-id-"+i, "109.105.110.5"+i, "India", "server-metadata : id: "+i+" , region : IN/Asia");
+			
+            for(int serverId = 0; serverId < 4; serverId++){
+				ConsistentHashDataNode<String> newServer = new Server<String>("server-id-"+serverId,
+                    "109.105.110.5" + serverId,
+                    "India",
+                    "server-metadata : id = " + serverId + ", region : IN/Asia");
 				servers.add(newServer);
 				cHash.addServer(newServer.data, newServer);	// Adding new server to circle
 			}
 
 			List <ConsistentHashDataNode<String>> data = new LinkedList<>();
-			for(int i = 0; i < 50; i++){
-				data.add(new ConsistentHashDataNode<String>("data-node-"+i));
+			for(int dataNodeId = 0; dataNodeId < 50; dataNodeId++){
+				data.add(new ConsistentHashDataNode<String>("data-node-"+dataNodeId));
 			}
-			
-			
-
 
 		}catch(RuntimeException ex){
-			System.err.println("Runtime Exception Stacktrace: " + ex.toString());
+			System.err.println("Computing Failed Stacktrace: " + ex.toString());
 		}
 	}
- 
 
-
-
-
-
-
-
-
-
-
-  /**
-   * Credit: MurmurHash from SMHasher written by Austin Appleby
-   * Ref   : https://en.wikipedia.org/wiki/MurmurHash
-   */
-  public Long getHash(String key){
-    ByteBuffer buf = ByteBuffer.wrap(key.getBytes());
-    int seed = 0x1234ABCD;
-    ByteOrder byteOrder = buf.order();
-    buf.order(ByteOrder.LITTLE_ENDIAN);
-    long m = 0xc6a4a7935bd1e995L;
-    int r  = 47;
-    long h = seed ^ (buf.remaining() * m);
-    long k;
-    while(buf.remaining() >= 8){
-      k = buf.getLong();
-      k *= m;
-      k ^= k >>> r;
-      k *= m;
-
-      h ^= k;
-      h *= m;
-    }
-    if(buf.remaining() > 0){
-      ByteBuffer finish = ByteBuffer.allocate(8).order(
-          											ByteOrder.LITTLE_ENDIAN);
-      // for big-endian version, do this first:
-      // finish.position(8-buf.remaining());
-      finish.put(buf).rewind();
-      h ^= finish.getLong();
-      h *= m;
-    }
-    h ^= h >>> r;
-    h *= m;
-    h ^= h >>> r;
-    buf.order(byteOrder);
-    return h;
-  }	
+    /**
+    * Credit: MurmurHash from SMHasher written by Austin Appleby
+    * Ref   : https://en.wikipedia.org/wiki/MurmurHash
+    */
+    public Long getHash(String key){
+        ByteBuffer buf = ByteBuffer.wrap(key.getBytes());
+        int seed = 0x1234ABCD;
+        ByteOrder byteOrder = buf.order();
+        buf.order(ByteOrder.LITTLE_ENDIAN);
+        long m = 0xc6a4a7935bd1e995L;
+        int r  = 47;
+        long h = seed ^ (buf.remaining() * m);
+        long k;
+        while(buf.remaining() >= 8){
+            k = buf.getLong();
+            k *= m;
+            k ^= k >>> r;
+            k *= m;
+            h ^= k;
+            h *= m;
+        }
+        if(buf.remaining() > 0){
+            ByteBuffer finish = ByteBuffer.allocate(8).order(
+              											ByteOrder.LITTLE_ENDIAN);
+            // for big-endian version, do this first:
+            // finish.position(8-buf.remaining());
+            finish.put(buf).rewind();
+            h ^= finish.getLong();
+            h *= m;
+        }
+        h ^= h >>> r;
+        h *= m;
+        h ^= h >>> r;
+        buf.order(byteOrder);
+        return h;
+    }	
 }
 
 
