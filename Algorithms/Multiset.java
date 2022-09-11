c86d23c7b25041f2fcb42f5a5549bdb9b1782439
Jaswant <developer.jaswant@gmail.com>
2022-09-11 13:41:57 +0530

    add multiset implementation

--- Algorithms/Multiset.java
+++ Algorithms/Multiset.java
@@ -5,27 +5,27 @@ import java.util.HashMap;
  * @use: holding frequency map, similar to multiset in c++
 */
 class MultiSet<K> {
-    private HashMap<K, Integer> multiSet = new HashMap<K, Integer>();
+	private HashMap<K, Integer> multiSet = new HashMap<K, Integer>();
 
-    int get(K key){
-        return multiSet.getOrDefault(key, 0);
-    }
+	int get(K key){
+		return multiSet.getOrDefault(key, 0);
+	}
 
-    void add(K key){
-        multiSet.put(key, get(key)+ 1);
-    }
+	void add(K key){
+		multiSet.put(key, get(key)+ 1);
+	}
 
-    void remove(K key){
-        int freq = get(key);
-        if(freq == 1){
-        multiSet.remove(key);
-        }else{
-        multiSet.put(key, freq - 1);
-        }
-    }
+	void remove(K key){
+		int freq = get(key);
+		if(freq == 1){
+			multiSet.remove(key);
+		}else{
+			multiSet.put(key, freq - 1);
+		}
+	}
 
-    @Override
-    public String toString(){
-        return multiSet.toString();
-    }
+	@Override
+	public String toString(){
+		return multiSet.toString();
+	}
 }
