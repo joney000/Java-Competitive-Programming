97533d2d102e00da7f4e991c6d9c3e2bb3ce6402
Jaswant Singh <joney000@users.noreply.github.com>
2022-09-11 13:38:32 +0530

    add multiset implementation (#11)
    * add multiset implementation
    
    * add multiset implementation

---
+++ Algorithms/Multiset.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+/* @author: jaswant developer.jaswant@gmail.com
+ * @algorithm: hashing
+ * @use: holding frequency map, similar to multiset in c++
+*/
+class MultiSet<K> {
+    private HashMap<K, Integer> multiSet = new HashMap<K, Integer>();
+
+    int get(K key){
+        return multiSet.getOrDefault(key, 0);
+    }
+
+    void add(K key){
+        multiSet.put(key, get(key)+ 1);
+    }
+
+    void remove(K key){
+        int freq = get(key);
+        if(freq == 1){
+        multiSet.remove(key);
+        }else{
+        multiSet.put(key, freq - 1);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return multiSet.toString();
+    }
+}
