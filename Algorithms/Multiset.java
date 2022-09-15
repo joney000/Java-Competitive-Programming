c3e27ba5b887cd25a86775bbc423b204fb7c3314
Jaswant <developer.jaswant@gmail.com>
2022-09-15 10:05:03 +0530

    add multiset size and isempty methods

--- Algorithms/Multiset.java
+++ Algorithms/Multiset.java
@@ -6,24 +6,34 @@ import java.util.HashMap;
 */
 class MultiSet<K> {
 	private HashMap<K, Integer> multiSet = new HashMap<K, Integer>();
-
+	private int size;
 	int get(K key){
 		return multiSet.getOrDefault(key, 0);
 	}
 
 	void add(K key){
+		size++;
 		multiSet.put(key, get(key)+ 1);
 	}
 
 	void remove(K key){
 		int freq = get(key);
+		size--;
 		if(freq == 1){
 			multiSet.remove(key);
 		}else{
 			multiSet.put(key, freq - 1);
 		}
 	}
-  
+	
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		return size == 0;
+	}
+	
 	@Override
 	public String toString(){
 		return multiSet.toString();
