3260d2fb743c044521c097ed842a2a89e7e331ab
joney000 <developer.jaswant@gmail.com>
2023-07-18 17:51:13 +0530

    add comparator use cases

--- Algorithms/ComparatorSnippet.java
+++ Algorithms/ComparatorSnippet.java
@@ -13,15 +13,27 @@ class Developer{
 	}
 }
 
+class DevComparator implements Comparator<Developer> {
+    @Override
+    public int compare(Developer firstValue, Developer secondValue){
+        return Integer.compare(firstValue.getAge(), secondValue.getAge());
+    }
+}
+
 public class ComparatorSnippet{
 
 	public static void main(String... args)throws Exception{
-		
+		// way - 1
+		// better if sorting criteria keeps changing
 		Developer[] devs = Collections.sort(listDevs, new Comparator<Developer>() {
 			@Override
 			public int compare(Developer o1, Developer o2) {
 				return o1.getAge() - o2.getAge();
 			}
 		});
+		// way - 2
+		devs = Collections.sort(listDevs, (Developer a , Developer b) -> Integer.compare(a.getAge(), b.getAge()));
+		// way - 3 
+		devs = Collections.sort(listDevs, new DevComparator());
 	}
 }
