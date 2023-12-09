58a1b90f7a631ff4a9b21a13fa2064fcf7c32560
joney000 <developer.jaswant@gmail.com>
2023-12-09 19:09:43 +0530

    add treap log n operations

--- Algorithms/Treap.java
+++ Algorithms/Treap.java
@@ -12,16 +12,16 @@ import java.math.*;
  *             https://threads-iiith.quora.com/Treaps-One-Tree-to-Rule-em-all-Part-2     
  */
 
-class Treap {
+class TreapNode {
 
   static Random random = new Random();
   int key;
   long prio;
-  Treap left;
-  Treap right;
+  TreapNode left;
+  TreapNode right;
   int count;
 
-  Treap(int key) {
+  TreapNode(int key) {
     this.key = key;
     this.prio = random.nextLong();;
     count = 1;
@@ -29,87 +29,34 @@ class Treap {
 }
 
 class TreapPair {
-  Treap left;
-  Treap right;
+  TreapNode left;
+  TreapNode right;
 
-  TreapPair(Treap left, Treap right) {
+  TreapPair(TreapNode left, TreapNode right) {
     this.left = left;
     this.right = right;
   }
 }
 
-class A{ 
+public static class TreapOperations {
 
-  static Random random = new Random();
-  private InputStream inputStream ;
-  private OutputStream outputStream ;
-  private FastReader in ;
-  private PrintWriter out ;
-  
-  private final int BUFFER = 100005;
-  
-  private int    auxInts[] = new int[BUFFER];
-  private long   auxLongs[] = new long[1];
-  private double auxDoubles[] = new double[1];
-  private char   auxChars[] = new char[1];
-  private final long mod = 1000000000+7;
-  private final int  INF  = Integer.MAX_VALUE;
-  private final long INF_L  = Long.MAX_VALUE / 10;
-
-  public A(){}
-  public A(boolean stdIO)throws FileNotFoundException{
-    // stdIO = false;
-    if(stdIO){
-      inputStream = System.in;
-      outputStream = System.out;
-    }else{
-      inputStream = new FileInputStream("input.txt");
-      outputStream = new FileOutputStream("output.txt");
-    }
-    in = new FastReader(inputStream);
-    out = new PrintWriter(outputStream);
-  }
-  
-  void run()throws Exception{
-    Treap treap = null;
-    Set<Integer> set = new TreeSet<Integer>();
-    for (int i = 0; i < 100000; i++) {
-      int x = random.nextInt(100000);
-      if (random.nextBoolean()) {
-        treap = remove(treap, x);
-        set.remove(x);
-      } else if (!set.contains(x)) {
-        treap = insert(treap, x);
-        set.add(x);
-      }
-      if (set.size() != getCount(treap))
-        throw new RuntimeException();
-    }
-
-    print(treap);
-  }
-  
-  void clear(){
-
-  }
-  
-  void updateCount(Treap root) {
+  public static void updateCount(TreapNode root) {
       root.count = 1 + getCount(root.left) + getCount(root.right);
   }
 
-  int getCount(Treap root) {
+  public static int getCount(TreapNode root) {
     return root == null ? 0 : root.count;
   }
 
-  TreapPair split(Treap root, int minRight){
+  public static TreapPair split(TreapNode root, int minRight){
     if(root == null)return new TreapPair(null, null);
-    if(root.key >= minRight){
+    if(root.key >= minRight) {
       TreapPair leftSplit = split(root.left, minRight);
       root.left = leftSplit.right;
       updateCount(root);
       leftSplit.right = root;
       return leftSplit;
-    }else{
+    } else {
       TreapPair rightSplit = split(root.right, minRight);
       root.right = rightSplit.left;
       updateCount(root);
@@ -118,7 +65,7 @@ class A{
     }
   }
 
-  Treap merge(Treap left, Treap right){
+  public static TreapNode merge(TreapNode left, TreapNode right){
     if(left == null)
       return right;
     if(right == null)
@@ -134,12 +81,12 @@ class A{
     }
   }
 
-  Treap insert(Treap root, int x){
+  public static TreapNode insert(TreapNode root, int x){
     TreapPair t = split(root, x);
-    return merge(merge(t.left, new Treap(x)), t.right);
+    return merge(merge(t.left, new TreapNode(x)), t.right);
   }
 
-  Treap remove(Treap root, int x){
+  public static TreapNode remove(TreapNode root, int x){
     if(root == null){
       return null;
     }
@@ -156,7 +103,7 @@ class A{
     }
   }
 
-  int kth(Treap root, int k){
+  public static int kth(TreapNode root, int k){
     if(k < getCount(root.left))
       return kth(root.left, k);
     else if(k > getCount(root.left))
@@ -164,374 +111,32 @@ class A{
     return root.key;
   }
 
-  void print(Treap root){
+  public static void printTree(TreapNode root){
     if(root == null)return;
-    print(root.left);
+    printTree(root.left);
     System.out.println(root.key);
-    print(root.right);
-  }
-
-  long gcd(long a, long b){
-    if(b == 0)return a;
-    return gcd(b, a % b);
-  }
-
-  long lcm(long a, long b){
-    if(a == 0 || b == 0)return 0;
-    return (a * b)/gcd(a, b);
-  }
-
-  long mulMod(long a, long b, long mod){
-    if(a == 0 || b == 0)return 0;
-    if(b == 1)return a;
-    long ans = mulMod(a, b/2, mod);
-    ans = (ans * 2) % mod;
-    if(b % 2 == 1)ans = (a + ans)% mod;
-    return ans;
-  }
-
-  long pow(long a, long b, long mod){
-    if(b == 0)return 1;
-    if(b == 1)return a;
-    long ans = pow(a, b/2, mod);
-    ans = (ans * ans);
-    if(ans >= mod)ans %= mod;
-
-    if(b % 2 == 1)ans = (a * ans);
-    if(ans >= mod)ans %= mod;
-
-    return ans;
-  }
-
-  // 20*20   nCr Pascal Table
-  long[][] ncrTable(){
-    long ncr[][] = new long[21][21];
-
-    for(int i = 0; i <= 20; i++){
-      ncr[i][0] = ncr[i][i] = 1L;
-    }
-
-    for(int j = 0; j <= 20; j++){
-      for(int i = j + 1; i <= 20; i++){
-        ncr[i][j] = ncr[i-1][j] + ncr[i-1][j-1];
-      }
-    }
-
-    return ncr;
-  }
-
-  int i()throws Exception{
-    return in.nextInt();
-  }
-
-  int[] is(int n)throws Exception{
-    for(int i=1 ; i <= n ;i++)auxInts[i] = in.nextInt();  
-      return auxInts;
-  }
-
-  long l()throws Exception{
-    return in.nextLong();
-  }
-
-  long[] ls(int n)throws Exception{
-    for(int i=1 ; i <= n ;i++)auxLongs[i] = in.nextLong();  
-      return auxLongs;
-  }
-
-  double d()throws Exception{
-    return in.nextDouble();
-  }
-
-  double[] ds(int n)throws Exception{
-    for(int i=1 ; i <= n ;i++)auxDoubles[i] = in.nextDouble();  
-      return auxDoubles;
-  }
-
-  char c()throws Exception{
-    return in.nextCharacter();
-  }
-
-  char[] cs(int n)throws Exception{
-    for(int i=1 ; i <= n ;i++)auxChars[i] = in.nextCharacter();  
-      return auxChars;
-  }
-
-  String s()throws Exception{
-    return in.nextLine();
-  }
-
-  BigInteger bi()throws Exception{
-    return in.nextBigInteger();
-  }
-
-  private void closeResources(){
-    out.flush();
-    out.close();
-    return;
-  }
-
-//  IMP: roundoff upto 2 digits 
-//  double roundOff = Math.round(a * 100.0) / 100.0;
-//                    or
-//  System.out.printf("%.2f", val);
-
-//  print upto 2 digits after decimal
-//  val = ((long)(val * 100.0))/100.0;    
-
-  public static void main(String[] args) throws java.lang.Exception{
-  
-    A driver = new A(true);
-    driver.run();
-    driver.closeResources();
+    printTree(root.right);
   }
 }
 
-class FastReader{
-
-  private boolean finished = false;
-
-  private InputStream stream;
-  private byte[] buf = new byte[4 * 1024];
-  private int curChar;
-  private int numChars;
-  private SpaceCharFilter filter;
-
-  public FastReader(InputStream stream){
-    this.stream = stream;
-  }
-
-  public int read(){
-    if (numChars == -1){
-      throw new InputMismatchException ();
-    }
-    if (curChar >= numChars){
-      curChar = 0;
-      try{
-        numChars = stream.read (buf);
-      } catch (IOException e){
-        throw new InputMismatchException ();
+class TreapTest { 
+  public static void main(String ...args){
+    TreapNode rootNode = null;
+    Random random = new Random();
+    Set<Integer> set = new TreeSet<Integer>();
+    for (int i = 0; i < 100000; i++) {
+      int x = random.nextInt(100000);
+      if (random.nextBoolean()) {
+        rootNode = TreapOperations.remove(rootNode, x);
+        set.remove(x);
+      } else if (!set.contains(x)) {
+        rootNode = TreapOperations.insert(rootNode, x);
+        set.add(x);
       }
-      if (numChars <= 0){
-        return -1;
-      }
+      if (set.size() != TreapOperations.getCount(rootNode))
+        throw new RuntimeException();
     }
-    return buf[curChar++];
-  }
 
-  public int peek(){
-    if (numChars == -1){
-      return -1;
-    }
-    if (curChar >= numChars){
-      curChar = 0;
-      try{
-        numChars = stream.read (buf);
-      } catch (IOException e){
-        return -1;
-      }
-      if (numChars <= 0){
-        return -1;
-      }
-    }
-    return buf[curChar];
-  }
-
-  public int nextInt(){
-    int c = read ();
-    while (isSpaceChar (c))
-      c = read ();
-    int sgn = 1;
-    if (c == '-'){
-      sgn = -1;
-      c = read ();
-    }
-    int res = 0;
-    do{
-      if(c==','){
-        c = read();
-      }
-      if (c < '0' || c > '9'){
-        throw new InputMismatchException ();
-      }
-      res *= 10;
-      res += c - '0';
-      c = read ();
-    } while (!isSpaceChar (c));
-    return res * sgn;
-  }
-
-  public long nextLong(){
-    int c = read ();
-    while (isSpaceChar (c))
-      c = read ();
-    int sgn = 1;
-    if (c == '-'){
-      sgn = -1;
-      c = read ();
-    }
-    long res = 0;
-    do{
-      if (c < '0' || c > '9'){
-        throw new InputMismatchException ();
-      }
-      res *= 10;
-      res += c - '0';
-      c = read ();
-    } while (!isSpaceChar (c));
-    return res * sgn;
-  }
-
-  public String nextString(){
-    int c = read ();
-    while (isSpaceChar (c))
-      c = read ();
-    StringBuilder res = new StringBuilder ();
-    do{
-      res.appendCodePoint (c);
-      c = read ();
-    } while (!isSpaceChar (c));
-    return res.toString ();
-  }
-
-  public boolean isSpaceChar(int c){
-    if (filter != null){
-      return filter.isSpaceChar (c);
-    }
-    return isWhitespace (c);
-  }
-
-  public static boolean isWhitespace(int c){
-    return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-  }
-
-  private String readLine0(){
-    StringBuilder buf = new StringBuilder ();
-    int c = read ();
-    while (c != '\n' && c != -1){
-      if (c != '\r'){
-        buf.appendCodePoint (c);
-      }
-      c = read ();
-    }
-    return buf.toString ();
-  }
-
-  public String nextLine(){
-    String s = readLine0 ();
-    while (s.trim ().length () == 0)
-      s = readLine0 ();
-    return s;
-  }
-
-  public String nextLine(boolean ignoreEmptyLines){
-    if (ignoreEmptyLines){
-      return nextLine ();
-    }else{
-      return readLine0 ();
-    }
-  }
-
-  public BigInteger nextBigInteger(){
-    try{
-      return new BigInteger (nextString ());
-    } catch (NumberFormatException e){
-      throw new InputMismatchException ();
-    }
-  }
-
-  public char nextCharacter(){
-    int c = read ();
-    while (isSpaceChar (c))
-      c = read ();
-    return (char) c;
-  }
-
-  public double nextDouble(){
-    int c = read ();
-    while (isSpaceChar (c))
-      c = read ();
-    int sgn = 1;
-    if (c == '-'){
-      sgn = -1;
-      c = read ();
-    }
-    double res = 0;
-    while (!isSpaceChar (c) && c != '.'){
-      if (c == 'e' || c == 'E'){
-        return res * Math.pow (10, nextInt ());
-      }
-      if (c < '0' || c > '9'){
-        throw new InputMismatchException ();
-      }
-      res *= 10;
-      res += c - '0';
-      c = read ();
-    }
-    if (c == '.'){
-      c = read ();
-      double m = 1;
-      while (!isSpaceChar (c)){
-        if (c == 'e' || c == 'E'){
-          return res * Math.pow (10, nextInt ());
-        }
-        if (c < '0' || c > '9'){
-          throw new InputMismatchException ();
-        }
-        m /= 10;
-        res += (c - '0') * m;
-        c = read ();
-      }
-    }
-    return res * sgn;
-  }
-
-  public boolean isExhausted(){
-    int value;
-    while (isSpaceChar (value = peek ()) && value != -1)
-      read ();
-    return value == -1;
-  }
-
-  public String next(){
-    return nextString ();
-  }
-
-  public SpaceCharFilter getFilter(){
-    return filter;
-  }
-
-  public void setFilter(SpaceCharFilter filter){
-    this.filter = filter;
-  }
-
-  public interface SpaceCharFilter{
-    public boolean isSpaceChar(int ch);
+    TreapOperations.printTree(rootNode);
   }
 }
-
-class Pair implements Comparable<Pair>{
-  public int a;
-  public int b;
-   
-  public Pair(){
-    this.a = 0;
-    this.b = 0;
-  }
-
-  public Pair(int a,int b){
-    this.a = a;
-    this.b = b;
-  }
-
-  public int compareTo(Pair p){
-    if(this.a == p.a){
-     return this.b - p.b;  
-   }
-   return this.a - p.a; 
-  }
-
-  @Override
-  public String toString(){
-    return "a = " + this.a + " b = " + this.b;
-  }
-} 
